1ddeb9a4b1787289181c5797a5870d66f81dca19
jplandrain <jplandrain@nidget.org>
2013-08-28 15:55:24 +0200

    Initial

---
+++ djeclipse/org.nidget.eclipse.djeclipse.plugin/src/org/nidget/eclipse/djeclipse/decompilers/exceptions/DecompilerException.java
@@ -0,0 +1,10 @@
+package org.nidget.eclipse.djeclipse.decompilers.exceptions;
+
+public class DecompilerException extends RuntimeException {
+
+	private static final long serialVersionUID = -9044511484233977570L;
+
+	public DecompilerException(Throwable cause) {
+		super(cause);
+	}
+}
