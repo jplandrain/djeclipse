aa53a373c73b785d0012673e3a3d3399c888cbaf
jplandrain <jplandrain@nidget.org>
2014-07-22 13:40:54 +0200

    Updated CFR library and related code to version 0.80 (from 0.38) Added Eclipse 4.5 profile (aka Eclipse Mars) Version set to 0.0.2-SNAPSHOT (from 0.0.1-SNAPSHOT)
    Signed-off-by: jplandrain <jplandrain@nidget.org>

--- org.nidget.eclipse.djeclipse.plugin/src/org/nidget/eclipse/djeclipse/decompilers/cfr/CFRDecompiler.java
+++ org.nidget.eclipse.djeclipse.plugin/src/org/nidget/eclipse/djeclipse/decompilers/cfr/CFRDecompiler.java
@@ -2,11 +2,14 @@ package org.nidget.eclipse.djeclipse.decompilers.cfr;
 
 import org.benf.cfr.reader.entities.ClassFile;
 import org.benf.cfr.reader.entities.Method;
+import org.benf.cfr.reader.state.DCCommonState;
+import org.benf.cfr.reader.state.TypeUsageCollector;
 import org.benf.cfr.reader.util.CannotLoadClassException;
 import org.benf.cfr.reader.util.ConfusedCFRException;
 import org.benf.cfr.reader.util.getopt.BadParametersException;
-import org.benf.cfr.reader.util.getopt.CFRState;
 import org.benf.cfr.reader.util.getopt.GetOptParser;
+import org.benf.cfr.reader.util.getopt.Options;
+import org.benf.cfr.reader.util.getopt.OptionsImpl;
 import org.benf.cfr.reader.util.output.Dumper;
 import org.benf.cfr.reader.util.output.ToStringDumper;
 import org.eclipse.core.runtime.IPath;
@@ -19,40 +22,42 @@ public class CFRDecompiler {
 		GetOptParser getOptParser = new GetOptParser();
 
 		try {
-			CFRState params = (CFRState)getOptParser.parse(new String[] {classPathStr}, CFRState.getFactory());
-			ClassFile c = params.getClassFileMaybePath(params.getFileName());
-
-			params.setClassFileVersion(c.getClassFileVersion());
+			Options options = (Options)getOptParser.parse(new String[] {classPathStr}, OptionsImpl.getFactory());		
+			DCCommonState dcCommonState = new DCCommonState(options);
+			Dumper dumper = new ToStringDumper();
+			
+			ClassFile c = dcCommonState.getClassFileMaybePath(options.getFileName());
+			dcCommonState.configureWith(c);
 			try {
-				c = params.getClassFile(c.getClassType());
-				if (params.analyseInnerClasses()) {
-					 c.loadInnerClasses(params);
-				}
-			} catch (CannotLoadClassException e) {
-			}
-
-			c.analyseTop(params);
-			Dumper result = new ToStringDumper();
-			String methname = params.getMethodName();
+				c = dcCommonState.getClassFile(c.getClassType());
+			} catch (CannotLoadClassException e) {}
+			c.loadInnerClasses(dcCommonState);
+			c.analyseTop(dcCommonState);
+			
+			TypeUsageCollector collectingDumper = new TypeUsageCollector(c);
+			c.collectTypeUsages(collectingDumper);
+			
+			String methname = options.getMethodName();
 			if (methname == null) {
-				c.dump(result);
+				c.dump(dumper);
 			} else {
 				try {
 					for (Method method : c.getMethodByName(methname)) {
-						method.dump(result, true);
+						method.dump(dumper, true);
 					}
 				} catch (NoSuchMethodException e) {
-					throw new BadParametersException("No such method '"
-							+ methname + "'.", CFRState.getFactory());
+					throw new BadParametersException("No such method '" + methname + "'.", OptionsImpl.getFactory());
 				}
 			}
-			return result.toString();
+			return dumper.toString();
 		} catch (CannotLoadClassException clce) {
 			throw new DecompilerException(clce);
 		} catch (BadParametersException bpe) {
 			throw new DecompilerException(bpe); 
 		} catch (ConfusedCFRException ccfre) {
 			throw new DecompilerException(ccfre);
+		} catch (RuntimeException rte) {
+			throw new DecompilerException(rte);
 		}
 	}
 }
